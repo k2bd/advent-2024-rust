50d79fe17e6c896c5c412146573c8c6659bb6c5c
Kevin Duff <kevinkelduff@gmail.com>
2024-12-01 11:24:16 +0000

    Day 1 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -22,13 +22,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (left, right) = parse_lists(input);
+
+    Some(
+        left.into_iter()
+            .map(|l| l * (right.iter().filter(|&&r| r == l).count() as u32))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
 
     #[test]
     fn test_parse_lists() {
@@ -47,6 +52,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
