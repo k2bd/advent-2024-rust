658ab8163df72089fa9166645603517466a6f416
Kevin Duff <kevinkelduff@gmail.com>
2024-12-01 11:20:41 +0000

    Day 1 Part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,52 @@
+use std::iter::zip;
+
+advent_of_code::solution!(1);
+
+fn parse_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut vals = line
+                .split_whitespace()
+                .map(|val| val.parse::<u32>().unwrap());
+            (vals.next().unwrap(), vals.next().unwrap())
+        })
+        .unzip()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut left, mut right) = parse_lists(input);
+    left.sort();
+    right.sort();
+    Some(zip(left, right).map(|(l, r)| (r).abs_diff(l)).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+
+    #[test]
+    fn test_parse_lists() {
+        let input = &advent_of_code::template::read_file("examples", DAY);
+        let result = parse_lists(input);
+
+        assert_eq!(result, (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3]))
+    }
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
