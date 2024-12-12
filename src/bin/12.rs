718bc05472fa27fe3765d014f0f9a986667a20b3
Kevin Duff <kevinkelduff@gmail.com>
2024-12-12 23:43:48 +0000

    D12p2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -25,6 +25,56 @@ impl GardenPlot {
     fn area(&self) -> usize {
         self.points.len()
     }
+
+    fn is_edge(&self, point: (isize, isize), dir: (isize, isize)) -> bool {
+        self.points.contains(&(point.0, point.1))
+            && !self.points.contains(&(point.0 + dir.0, point.1 + dir.1))
+    }
+    fn is_right_edge(&self, point: (isize, isize)) -> bool {
+        self.is_edge(point, (0, 1))
+    }
+    fn is_left_edge(&self, point: (isize, isize)) -> bool {
+        self.is_edge(point, (0, -1))
+    }
+    fn is_top_edge(&self, point: (isize, isize)) -> bool {
+        self.is_edge(point, (-1, 0))
+    }
+    fn is_bottom_edge(&self, point: (isize, isize)) -> bool {
+        self.is_edge(point, (1, 0))
+    }
+
+    fn sides(&self) -> usize {
+        let right_sides = self
+            .points
+            .iter()
+            .filter(|&&(row, col)| {
+                self.is_right_edge((row, col)) && !self.is_right_edge((row + 1, col))
+            })
+            .count();
+        let left_sides = self
+            .points
+            .iter()
+            .filter(|&&(row, col)| {
+                self.is_left_edge((row, col)) && !self.is_left_edge((row - 1, col))
+            })
+            .count();
+        let top_sides = self
+            .points
+            .iter()
+            .filter(|&&(row, col)| {
+                self.is_top_edge((row, col)) && !self.is_top_edge((row, col + 1))
+            })
+            .count();
+        let bottom_sides = self
+            .points
+            .iter()
+            .filter(|&&(row, col)| {
+                self.is_bottom_edge((row, col)) && !self.is_bottom_edge((row, col - 1))
+            })
+            .count();
+
+        right_sides + left_sides + top_sides + bottom_sides
+    }
 }
 
 fn parse_input(input: &str) -> Vec<GardenPlot> {
@@ -88,7 +138,12 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    Some(
+        parse_input(input)
+            .into_iter()
+            .map(|plot| plot.area() * plot.sides())
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -114,6 +169,17 @@ mod tests {
     #[case(GardenPlot{ points: HashSet::from([(0, 0), (1, 0), (1, 1), (2, 1)]) }, 10)]
     fn test_perimeter(#[case] plot: GardenPlot, #[case] expected_perimeter: usize) {
         assert_eq!(plot.perimeter(), expected_perimeter);
+    }
+
+    #[rstest]
+    #[case(GardenPlot{ points: HashSet::from([(3, 3)]) }, 4)]
+    #[case(GardenPlot{ points: HashSet::from([(3, 3), (3, 4), (4, 3), (4, 4)]) }, 4)]
+    #[case(GardenPlot{ points: HashSet::from([(0, 0), (0, 1), (0, 2)]) }, 4)]
+    #[case(GardenPlot{ points: HashSet::from([(0, 0), (0, 1), (0, 2), (0, 3)]) }, 4)]
+    #[case(GardenPlot{ points: HashSet::from([(0, 0), (1, 0), (2, 0), (3, 0)]) }, 4)]
+    #[case(GardenPlot{ points: HashSet::from([(0, 0), (1, 0), (1, 1), (2, 1)]) }, 8)]
+    fn test_sides(#[case] plot: GardenPlot, #[case] expected_sides: usize) {
+        assert_eq!(plot.sides(), expected_sides);
     }
 
     #[test]
@@ -145,7 +211,15 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 1,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(80));
+    }
+
+    #[test]
+    fn test_day_12_part_two_from_example_2() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(436));
     }
 
     #[test]
@@ -153,7 +227,7 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 3,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(1206));
     }
 
     #[test]
@@ -161,7 +235,7 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 4,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(236));
     }
 
     #[test]
@@ -169,6 +243,6 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 5,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(368));
     }
 }
