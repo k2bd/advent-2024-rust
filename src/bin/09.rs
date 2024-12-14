e132b0ddd7a08f1c1576bdb4e54958a3ed651bec
Kevin Duff <kevinkelduff@gmail.com>
2024-12-14 14:11:09 +0000

    Dat 14 part 1

---
+++ src/bin/09.rs
@@ -0,0 +1,148 @@
+use std::ops::Index;
+
+advent_of_code::solution!(9);
+
+#[derive(Debug, PartialEq)]
+struct FileSystem {
+    blocks: Vec<Option<usize>>,
+}
+
+impl FileSystem {
+    fn from_dense_rep(input: &str) -> Self {
+        let mut current_id = 0;
+        let mut blocks: Vec<Option<usize>> = Vec::new();
+        let mut pushing_file = true;
+        input.trim().chars().for_each(|c| {
+            let quantity = c.to_digit(10).unwrap();
+            (0..quantity).for_each(|_| {
+                blocks.push(if pushing_file { Some(current_id) } else { None });
+            });
+            if pushing_file {
+                current_id += 1
+            };
+            pushing_file = !pushing_file;
+        });
+
+        Self { blocks }
+    }
+
+    fn fragged(&self) -> Vec<usize> {
+        let mut left_index = 0;
+        let mut right_index = self.blocks.len() - 1;
+
+        let mut result = Vec::new();
+
+        while left_index <= right_index {
+            if let Some(value) = self.blocks[left_index] {
+                result.push(value);
+            } else {
+                while self.blocks[right_index].is_none() {
+                    right_index -= 1;
+                }
+                result.push(self.blocks[right_index].unwrap());
+                right_index -= 1;
+            }
+
+            left_index += 1;
+        }
+
+        result
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(
+        FileSystem::from_dense_rep(input)
+            .fragged()
+            .into_iter()
+            .enumerate()
+            .map(|(index, value)| index * value)
+            .sum(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_dense_rep() {
+        let file_system = FileSystem::from_dense_rep("2333133121414131402");
+        assert_eq!(
+            file_system,
+            FileSystem {
+                blocks: vec![
+                    Some(0),
+                    Some(0),
+                    None,
+                    None,
+                    None,
+                    Some(1),
+                    Some(1),
+                    Some(1),
+                    None,
+                    None,
+                    None,
+                    Some(2),
+                    None,
+                    None,
+                    None,
+                    Some(3),
+                    Some(3),
+                    Some(3),
+                    None,
+                    Some(4),
+                    Some(4),
+                    None,
+                    Some(5),
+                    Some(5),
+                    Some(5),
+                    Some(5),
+                    None,
+                    Some(6),
+                    Some(6),
+                    Some(6),
+                    Some(6),
+                    None,
+                    Some(7),
+                    Some(7),
+                    Some(7),
+                    None,
+                    Some(8),
+                    Some(8),
+                    Some(8),
+                    Some(8),
+                    Some(9),
+                    Some(9),
+                ]
+            }
+        )
+    }
+
+    #[test]
+    fn test_fragged() {
+        let file_system = FileSystem::from_dense_rep("2333133121414131402");
+        assert_eq!(
+            file_system.fragged(),
+            vec![
+                0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6
+            ],
+        )
+    }
+
+    #[test]
+    fn test_day_9_part_one_from_example() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1928));
+    }
+
+    #[test]
+    fn test_day_9_part_two_from_example() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
---
+++ src/bin/14.rs
@@ -0,0 +1,221 @@
+use std::ops::{Add, Div, Mul, Rem, Sub};
+
+advent_of_code::solution!(14);
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, PartialOrd, Ord)]
+struct Point(isize, isize);
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Mul<isize> for Point {
+    type Output = Self;
+
+    fn mul(self, rhs: isize) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+impl Rem<Point> for Point {
+    type Output = Self;
+
+    fn rem(self, rhs: Point) -> Self::Output {
+        Self(self.0.rem_euclid(rhs.0), self.1.rem_euclid(rhs.1))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Robot {
+    position: Point,
+    velocity: Point,
+}
+
+impl Robot {
+    fn from_line(line: &str) -> Self {
+        let (pos_part, vel_part) = line.split_once(" ").unwrap();
+        let (pos_x_part, pos_y_part) = pos_part
+            .strip_prefix("p=")
+            .unwrap()
+            .split_once(",")
+            .unwrap();
+        let (vel_x_part, vel_y_part) = vel_part
+            .strip_prefix("v=")
+            .unwrap()
+            .split_once(",")
+            .unwrap();
+        Self {
+            position: Point(pos_x_part.parse().unwrap(), pos_y_part.parse().unwrap()),
+            velocity: Point(vel_x_part.parse().unwrap(), vel_y_part.parse().unwrap()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct RoboRoom {
+    robots: Vec<Robot>,
+    size: Point,
+}
+
+impl RoboRoom {
+    fn from_input(input: &str, width: usize, height: usize) -> Self {
+        Self {
+            robots: input.lines().map(|line| Robot::from_line(line)).collect(),
+            size: Point(width as isize, height as isize),
+        }
+    }
+
+    fn positions_at_timepoint(&self, time: isize) -> Vec<Point> {
+        self.robots
+            .iter()
+            .map(|robot| (robot.position + robot.velocity * time) % self.size)
+            .collect()
+    }
+
+    fn safety_factor_at_timepoint(&self, time: isize) -> usize {
+        let positions = self.positions_at_timepoint(time);
+
+        [
+            ((0, self.size.0 / 2), (0, self.size.1 / 2)),
+            ((0, self.size.0 / 2), ((self.size.1 / 2) + 1, self.size.1)),
+            (((self.size.0 / 2) + 1, self.size.0), (0, self.size.1 / 2)),
+            (
+                ((self.size.0 / 2) + 1, self.size.0),
+                ((self.size.1 / 2) + 1, self.size.1),
+            ),
+        ]
+        .into_iter()
+        .map(|(((x_min, x_max), (y_min, y_max)))| {
+            positions
+                .iter()
+                .filter(|&&Point(x, y)| x >= x_min && x < x_max && y >= y_min && y < y_max)
+                .count()
+        })
+        .fold(1, |acc, value| acc * value)
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    Some(RoboRoom::from_input(input, 101, 103).safety_factor_at_timepoint(100))
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let robo_room =
+            RoboRoom::from_input(&advent_of_code::template::read_file("examples", DAY), 11, 7);
+        assert_eq!(
+            robo_room,
+            RoboRoom {
+                robots: vec![
+                    Robot {
+                        position: Point(0, 4),
+                        velocity: Point(3, -3)
+                    },
+                    Robot {
+                        position: Point(6, 3),
+                        velocity: Point(-1, -3)
+                    },
+                    Robot {
+                        position: Point(10, 3),
+                        velocity: Point(-1, 2)
+                    },
+                    Robot {
+                        position: Point(2, 0),
+                        velocity: Point(2, -1)
+                    },
+                    Robot {
+                        position: Point(0, 0),
+                        velocity: Point(1, 3)
+                    },
+                    Robot {
+                        position: Point(3, 0),
+                        velocity: Point(-2, -2)
+                    },
+                    Robot {
+                        position: Point(7, 6),
+                        velocity: Point(-1, -3)
+                    },
+                    Robot {
+                        position: Point(3, 0),
+                        velocity: Point(-1, -2)
+                    },
+                    Robot {
+                        position: Point(9, 3),
+                        velocity: Point(2, 3)
+                    },
+                    Robot {
+                        position: Point(7, 3),
+                        velocity: Point(-1, 2)
+                    },
+                    Robot {
+                        position: Point(2, 4),
+                        velocity: Point(2, -3)
+                    },
+                    Robot {
+                        position: Point(9, 5),
+                        velocity: Point(-3, -3)
+                    },
+                ],
+                size: Point(11, 7),
+            }
+        )
+    }
+
+    #[test]
+    fn test_positions_at_timepoint() {
+        let result =
+            RoboRoom::from_input(&advent_of_code::template::read_file("examples", DAY), 11, 7)
+                .positions_at_timepoint(100);
+        assert_eq!(
+            result,
+            vec![
+                Point(3, 5),
+                Point(5, 4),
+                Point(9, 0),
+                Point(4, 5),
+                Point(1, 6),
+                Point(1, 3),
+                Point(6, 0),
+                Point(2, 3),
+                Point(0, 2),
+                Point(6, 0),
+                Point(4, 5),
+                Point(6, 6),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_safety_factor_at_timepoint() {
+        let result =
+            RoboRoom::from_input(&advent_of_code::template::read_file("examples", DAY), 11, 7)
+                .safety_factor_at_timepoint(100);
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn test_day_14_part_two_from_example() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
